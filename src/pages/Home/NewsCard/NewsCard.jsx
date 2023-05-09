0ceb8134358ee0d2eae49e50e66753a391315a5e
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-05-09 23:30:08 +0600

    NewsCard bookmark,share,eye icons and total view added

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,19 +1,26 @@
 import moment from 'moment';
 import React from 'react';
 import { Card, Image } from 'react-bootstrap';
+import { FaEye, FaRegBookmark, FaShareAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const NewsCard = ({ news }) => {
-    const { _id, title, details, image_url, author } = news;
+    const { _id, title, details, image_url, author, rating, total_view } = news;
     return (
         <Card className="mb-4">
-            <Card.Header className='d-flex'>
+
+            <Card.Header className='d-flex align-items-center'>
                 <Image style={{ height: '40px' }} src={author.img} roundedCircle />
-                <div className='ps-2'>
+                <div className='ps-2 flex-grow-1'>
                     <p className='mb-0'>{author?.name}</p>
                     <p><small> {moment(author?.published_date).format('YYYY-MM-D')}</small></p>
                 </div>
+                <div>
+                    <FaRegBookmark></FaRegBookmark>
+                    <FaShareAlt></FaShareAlt>
+                </div>
             </Card.Header>
+
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Img variant="top" src={image_url} />
@@ -21,7 +28,18 @@ const NewsCard = ({ news }) => {
                     {details.length < 250 ? <>{details}</> : <>{details.slice(0, 250)}... <Link to={`/news/${_id}`}>Read More</Link> </>}
                 </Card.Text>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+
+            <Card.Footer className="text-muted d-flex">
+                <div className='flex-grow-1'>
+                    <span>
+                        {rating?.number}
+                    </span>
+                </div>
+                <div>
+                    <FaEye></FaEye> {total_view}
+                </div>
+            </Card.Footer>
+
         </Card>
     );
 };
