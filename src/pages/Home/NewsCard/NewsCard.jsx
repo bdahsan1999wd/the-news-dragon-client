5e250238a0023932e3c84fc8f35dd51a58f8879f
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-05-09 23:09:16 +0600

    Load News Category Data From Server and Style News Card Component

---
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -0,0 +1,29 @@
+import moment from 'moment';
+import React from 'react';
+import { Card, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const NewsCard = ({ news }) => {
+    const { _id, title, details, image_url, author } = news;
+    return (
+        <Card className="mb-4">
+            <Card.Header className='d-flex'>
+                <Image style={{ height: '40px' }} src={author.img} roundedCircle />
+                <div className='ps-2'>
+                    <p className='mb-0'>{author?.name}</p>
+                    <p><small> {moment(author?.published_date).format('YYYY-MM-D')}</small></p>
+                </div>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Text>
+                    {details.length < 250 ? <>{details}</> : <>{details.slice(0, 250)}... <Link to={`/news/${_id}`}>Read More</Link> </>}
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        </Card>
+    );
+};
+
+export default NewsCard;
