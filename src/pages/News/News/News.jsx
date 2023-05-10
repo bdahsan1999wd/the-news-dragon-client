e77b7b641fecdf81767cdf2171019795c96faec3
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-05-10 10:48:24 +0600

    Editors Insights card added

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { FaArrowLeft } from 'react-icons/fa';
 import { Link, useLoaderData } from 'react-router-dom';
+import EditorsInsights from '../EditorsInsights/EditorsInsights';
 
 const News = () => {
 
@@ -9,17 +10,22 @@ const News = () => {
     const { title, details, image_url, category_id } = news;
 
     return (
-        <Card>
-            <Card.Img variant="top" src={image_url} />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                    {details}
-                </Card.Text>
-                <Link to={`/category/${category_id}`}>
-                    <Button variant="danger"> <FaArrowLeft></FaArrowLeft> All News In This Category</Button></Link>
-            </Card.Body>
-        </Card>
+        <div>
+            <Card>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                    <Link to={`/category/${category_id}`}>
+                        <Button variant="danger"> <FaArrowLeft></FaArrowLeft> All News In This Category</Button></Link>
+                </Card.Body>
+            </Card>
+
+            <EditorsInsights></EditorsInsights>
+
+        </div>
     );
 };
 
