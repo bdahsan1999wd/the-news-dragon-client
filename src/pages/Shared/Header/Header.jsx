c69e22b8209185d637c0d68ef3d4565edc8b3ddf
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-05-11 21:00:43 +0600

    set Auth Context and Auth Provider

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
+
+    const { user } = useContext(AuthContext);
+
     return (
         <Container className='mt-4'>
             <div className='text-center'>
@@ -33,9 +38,16 @@ const Header = () => {
                             <Nav.Link href="#pricing">Career</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            {user && <Nav.Link href="#deets">
+                                <FaUserCircle style={{ fontSize: '2rem' }}></FaUserCircle>
+                            </Nav.Link>}
                             <Nav.Link eventKey={2} href="#memes">
-                                <Button variant="secondary">Login</Button>
+                                {user ?
+                                    <Button variant="secondary">Logout</Button> :
+                                    <Link to="/login">
+                                    <Button variant="secondary">Login</Button>
+                                    </Link>
+                                }
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
