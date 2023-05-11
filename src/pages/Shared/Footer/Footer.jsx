6499ee740b1310b1f37bb82e9cf69283e1a4e3ca
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-05-11 19:42:22 +0600

    nav home link added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p className='text-center'><small>© Copyright of my news</small></p>
+            <p className='text-center'><small>© Copyright of my news 2023</small></p>
         </div>
     );
 };
