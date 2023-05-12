341b77b315b2612dc71f47d74f57f343e3da100b
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-05-12 16:39:59 +0600

    Login and Register Form added

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is Register page...</h2>
-        </div>
+        <Container className='w-50 mx-auto'>
+            <h3>Please Register...</h3>
+            <Form>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="text" name="name" required placeholder="Enter Your Name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control type="text" name="photo" required placeholder="Chose a Photo" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" required placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" required placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" name="accept" label="Accept Terms and Conditions" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+
+                <br />
+
+                <Form.Text className="text-danger">
+                    Already Have an Account? <Link to="/login">Login</Link>
+                </Form.Text>
+
+                <Form.Text className="text-success">
+
+                </Form.Text>
+
+                <Form.Text className="text-danger">
+
+                </Form.Text>
+
+            </Form>
+        </Container>
     );
 };
 
