c5d2c8f6fd69e7b45f2cd9f2450478c8fb01224a
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-05-12 16:07:15 +0600

    Create Login Layout Component for Login and Registrationr Route AND Create Login and Register Component

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -8,7 +8,7 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
 
-    const user = {displayName: 'Muri Kha...'}
+    const user = null;
 
     const authInfo = {
         user
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,22 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
 import Category from "../pages/Home/Category/Category";
 import NewsLayOut from "../layouts/NewsLayOut";
 import News from "../pages/News/News/News";
+import LoginLayOut from "../layouts/LoginLayOut";
+import Login from "../pages/Login/Login/Login";
+import Register from "../pages/Login/Register/Register";
 
 const router = createBrowserRouter([
+
     {
         path: '/',
-        element: <Main></Main>,
+        element: <LoginLayOut></LoginLayOut>,
         children: [
             {
                 path: '/',
-                element: <Category></Category>,
-                loader: () => fetch(`http://localhost:5000/news/`)
+                element: <Navigate to="/category/0"></Navigate>
             },
             {
-                path: '/category/:id',
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: 'register',
+                element: <Register></Register>
+            }
+        ]   
+    },
+    {
+        path: 'category',
+        element: <Main></Main>,
+        children: [
+            {
+                path: ':id',
                 element: <Category></Category>,
                 loader: ({ params }) => fetch(`http://localhost:5000/categories/${params.id}`)
 
