6db62cc2d6eb360d1403a39bfde169e933d65c50
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-05-15 00:33:41 +0600

    Fixed Warning!!! Problem React Rating

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -2,8 +2,11 @@ import moment from 'moment';
 import React from 'react';
 import { Card, Image } from 'react-bootstrap';
 import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from 'react-icons/fa';
-import Rating from 'react-rating';
 import { Link } from 'react-router-dom';
+
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+
 
 const NewsCard = ({ news }) => {
     const { _id, title, details, image_url, author, rating, total_view } = news;
@@ -31,21 +34,17 @@ const NewsCard = ({ news }) => {
             </Card.Body>
 
             <Card.Footer className="text-muted d-flex">
-                <div className='flex-grow-1'>
-                    <span>
-                        <Rating
-                            placeholderRating={rating.number}
-                            readonly
-                            emptySymbol={<FaRegStar></FaRegStar>}
-                            placeholderSymbol={<FaStar className='text-warning'></FaStar>}
-                            fullSymbol={<FaStar></FaStar>}
-                        ></Rating>
-                        <span className='ms-3'>{rating?.number}</span>
-                    </span>
+
+                <div className='flex-grow-1 d-flex align-items-center'>
+                    <Rating style={{ maxWidth: 150 }} value={Math.round(rating?.number || 0)} readOnly />
+
+                    <span className='ms-3'>{rating?.number}</span>
                 </div>
+
                 <div>
                     <FaEye></FaEye> {total_view}
                 </div>
+
             </Card.Footer>
 
         </Card>
